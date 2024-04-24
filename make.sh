738b3d139d9b94db39540b746a12d904fcf6393d
jrgdrs <github@joerg-drees.de>
2024-04-24 22:48:18 +0200

    v002

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 gftools builder sources/config.yaml  
 
-echo '-- PNG Previews'
+echo '-- create png preview images'
+
 python3 documentation/imageTTF.py --ttf fonts/ttf/Promptus-ExtraLight.ttf --output documentation/image-ExtraLight.png
 python3 documentation/imageTTF.py --ttf fonts/ttf/Promptus-Light.ttf --output documentation/image-Light.png
 python3 documentation/imageTTF.py --ttf fonts/ttf/Promptus-Regular.ttf --output documentation/image-Regular.png
@@ -8,6 +9,19 @@ python3 documentation/imageTTF.py --ttf fonts/ttf/Promptus-SemiBold.ttf --output
 python3 documentation/imageTTF.py --ttf fonts/ttf/Promptus-Bold.ttf --output documentation/image-Bold.png
 python3 documentation/imageTTF.py --ttf fonts/ttf/Promptus-Black.ttf --output documentation/image-Black.png
 
-echo '-- PDF Samples'
-node print.js /documentation/Specimen.html
-node print.js /documentation/Woolf.html
+echo '-- create pdfs for tests'
+
+for i in ExtraLight Black; do 
+  node print.js qa/Glyphs.html fonts/ttf/Promptus-$i.ttf qa/1-Glyphs-$i.pdf
+  node print.js qa/Spacing.html fonts/ttf/Promptus-$i.ttf qa/2-Spacing-$i.pdf
+  node print.js qa/Kerning.html fonts/ttf/Promptus-$i.ttf qa/3-Kerning-$i.pdf
+  node print.js qa/Frequency.html fonts/ttf/Promptus-$i.ttf qa/4-Frequency-$i.pdf
+  node print.js qa/Kafka.html fonts/ttf/Promptus-$i.ttf qa/5-Kafka-$i.pdf
+  node print.js qa/News.html fonts/ttf/Promptus-$i.ttf qa/6-News-$i.pdf
+  node print.js qa/Diacritics.html fonts/ttf/Promptus-$i.ttf qa/7-Diacritics-$i.pdf
+done
+
+echo '-- create specimen for documentation'
+
+node print.js documentation/Specimen.html
+node print.js documentation/Woolf.html
